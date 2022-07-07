5953b1d5c6ed030b4378c115bcbee552aa43e1f3
Diego Perez <eazel7@gmail.com>
2022-07-07 12:24:01 +0200

    format SP parameters as the boss wants

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var parser = new TSql150Parser(true);
+
+        var reader = new StreamReader(File.OpenRead("sample.sql"));
+
+        IList<ParseError> errors;
+
+        var tokenList = parser.Parse(reader, out errors);
+
+        var visitor = new MyVisitor();
+
+        tokenList.Accept(visitor);
+        Console.WriteLine("GO");
+    }
+}
+
+public class MyVisitor : TSqlFragmentVisitor
+{
+    int? headerEndsAtOffset = null;
+    public override void Visit(TSqlStatement node)
+    {
+        if (node is CreateProcedureStatement)
+        {
+            var createProcedureStatement = node as CreateProcedureStatement;
+
+            headerEndsAtOffset = createProcedureStatement.ScriptTokenStream
+            .Where(t => t.TokenType == TSqlTokenType.As)
+            .Select(t => t.Offset + t.Text.Length)
+            .Last();
+
+            Console.WriteLine($"CREATE PROCEDURE {createProcedureStatement.ProcedureReference.Name.Identifiers.Single().Value}");
+
+            Console.Write(String.Join(",\n", createProcedureStatement.Parameters.Select(p => $"  {p.VariableName.Value} {p.DataType.Name.Identifiers[0].Value}")));
+
+            Console.WriteLine("\nAS");
+        } else {
+            foreach(var other in node.ScriptTokenStream) {
+                if (headerEndsAtOffset != null && other.Offset > headerEndsAtOffset.Value) {
+                    Console.Write(other.Text);
+                }
+            }
+        }
+
+        base.Visit(node);
+    }
+
+}
---
+++ sample.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE some_stored_procedure_name
+@p1 int,
+@p2 int,
+@p3 int
+AS
+
+-- wefewf
+select getdate()
+
+-- fewfewf
+
+
+
+-- fwefew
+GO
